dbffefc795e60e2fb0032b4ce6af2d3d2e2843ef
lsabor <lukesabor@gmail.com>
2022-03-17 20:26:09 -0400

    made basic graph class

--- modules/graphs.py
+++ modules/graphs.py
@@ -1,10 +1,34 @@
-# This module holds basic math functions
+# This module holds basic graph functions
 
 
+class Node:
+    
+    def __init__(self,value=None):
+        self.value = value
 
-class GNode:
-    pass
+    def __repr__(self):
+        return str(self.value)
+
+class Edge:
+
+    def __init__(self,length = 0,n1 = None,n2 = None):
+        self.length = length
+        self.n1 = n1
+        self.n2 = n2
+
+    def __repr__(self):
+        return f'{self.n1} -{self.length if self.length else ""}> {self.n2}'
 
 
+class Graph:
+
+    def __init__(self,edges = None):
+        self.edges = edges if edges else []
+
+    def __repr__(self):
+        output = ''
+        for edge in self.edges:
+            output += f'{edge}\n'
+        return output
 
 

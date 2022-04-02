25bc5210bbe3e6902fae5debf1ef0da9859868a5
lsabor <lukesabor@gmail.com>
2022-04-02 15:23:43 -0400

    slightly changes parsing, solves 0067 (extension of 0018)

--- modules/parsing.py
+++ modules/parsing.py
@@ -3,6 +3,7 @@
 
 def read_array_str(array: str) -> list:
     # returns a list of lists, each contained list is a row of array
+    array = array.strip()
     arr = array.split('\n')
     arr = [[int(value) for value in a.strip().split(' ')] for a in arr]
     return(arr)

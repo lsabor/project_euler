605c6684875826b1af63d7d3e2e6d6905b9aff89
Luke Sabor <lukesabor@gmail.com>
2022-09-09 00:04:40 -0400

    sovles 076

---
+++ 000-100/70s/076_10_Counting_Summations.py
@@ -0,0 +1,62 @@
+"""
+Counting summations
+Problem 76
+
+It is possible to write five as a sum in exactly six different ways:
+
+4 + 1
+3 + 2
+3 + 1 + 1
+2 + 2 + 1
+2 + 1 + 1 + 1
+1 + 1 + 1 + 1 + 1
+
+How many different ways can one hundred be written as a sum of at least two positive integers?
+
+Link: https://projecteuler.net/problem=76
+
+Date solved:  
+2022/09/08
+Solved with DeMehr Haywood
+"""
+
+# TODO: refactor for speed
+
+ANSWER = 190569291
+
+# imports
+
+from functools import lru_cache
+
+# solution
+
+
+@lru_cache
+def H(n, a):
+    if a == 1 or n == a:
+        return 1
+    if a > n:
+        return 0
+
+    return sum([H(n - a, b) for b in range(1, a + 1)])
+
+
+def f(n):
+    return sum([H(n, a) for a in range(1, n + 1)])
+
+
+def solution(bypass=True):
+    if bypass:
+        return ANSWER
+    num = 100
+    return f(num) - 1
+
+
+if __name__ == "__main__":
+    from time import perf_counter
+
+    t0 = perf_counter()
+    sol = solution(bypass=False)
+    t1 = perf_counter()
+    print(f"solution = {sol} in {t1-t0: 0.4f} seconds")
+    print("answer   =", ANSWER)

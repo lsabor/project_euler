cc259c20ae551d4a42b8f08250b439c132e48943
lsabor <lukesabor@gmail.com>
2022-10-06 12:10:10 -0400

    renames fixed broken problem 067

--- 000-100/60s/067_05_Maximum_path_sum_II.py
+++ 000-100/60s/067_05_Maximum_path_sum_II.py
@@ -3,7 +3,8 @@
 
 Problem 67
 
-By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum 
+total from top to bottom is 23.
 
 3
 7 4
@@ -12,9 +13,12 @@ By starting at the top of the triangle below and moving to adjacent numbers on t
 
 That is, 3 + 7 + 4 + 9 = 23.
 
-Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
+Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 
+15K text file containing a triangle with one-hundred rows.
 
-NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 2^99 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
+NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve 
+this problem, as there are 2^99 altogether! If you could check one trillion (1012) routes every second it 
+would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
 
 
 
